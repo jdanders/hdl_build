a13903b1c50d0f327826475c0cc6321a5d04f6b8
jdanders <jordan.the.anderson@gmail.com>
2021-03-23 10:43:14 -0600

    Add modelsim to unit test script

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-make comp
+SIM_TOOL=questa make -e comp
+SIM_TOOL=modelsim make -e comp
 SYNTH_OVERRIDE=y make project
-if [ ! $(ls -1 bld/deps | wc -l) -eq 24 ]; then
+if [ ! $(ls -1 bld/deps | wc -l) -eq 36 ]; then
     echo "The number of deps don't match expected"
     exit 1
+fi
+if ! ls bld/deps/*questa* > /dev/null; then
+    echo "No questa dependencies found"
+fi
+if ! ls bld/deps/*modelsim* > /dev/null; then
+    echo "No modelsim dependencies found"
+fi
+if ! ls bld/deps/*quartus* > /dev/null; then
+    echo "No quartus dependencies found"
 fi
 make cleanall
